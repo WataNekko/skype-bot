66b97784d854cd51e99aac643f413a6faa7fcb40
WataNekko <88185666+WataNekko@users.noreply.github.com>
2024-09-27 00:49:22 +0700

    Cache rsp cmds object in memory

--- bot.py
+++ bot.py
@@ -12,8 +12,9 @@ from dateutil import tz
 import traceback
 import logging
 import json
+from typing import NotRequired, Type, TypedDict
 
-RESPONSE_COMMAND_FILE_PATH = path.join(path.dirname(__file__), "response_cmd.json")
+RESPONSE_COMMANDS_FILE_PATH = path.join(path.dirname(__file__), "response_cmd.json")
 
 
 def handle_program_singleton():
@@ -43,27 +44,70 @@ def parse_skype_msg(skype_msg: SkypeMsg) -> tuple[None | Tag, BeautifulSoup]:
     return tag.extract() if tag.name == "quote" else None, msg
 
 
-def read_response_cmd(name: str | None) -> dict:
-    response_cmd = {}
-    if path.isfile(RESPONSE_COMMAND_FILE_PATH):
-        with open(RESPONSE_COMMAND_FILE_PATH) as f:
+class ResponseCommand(TypedDict):
+    chats: list[str]
+    triggers: list[str]
+
+    class Response(TypedDict):
+        quote: NotRequired[str]
+        file: NotRequired[str]
+
+    response: Response
+
+
+ResponseCommands = dict[str, ResponseCommand]
+
+
+def read_response_cmds() -> ResponseCommands:
+    rsp_cmds = {}
+    if path.isfile(RESPONSE_COMMANDS_FILE_PATH):
+        with open(RESPONSE_COMMANDS_FILE_PATH) as f:
             try:
-                response_cmd = json.load(f)
+                rsp_cmds = json.load(f)
             except:
                 pass
 
-    if name is not None:
-        response_cmd.setdefault(name, {})
-
-    return response_cmd
+    return rsp_cmds
 
 
-def write_response_cmd(response_cmd: dict):
-    with open(RESPONSE_COMMAND_FILE_PATH, "w") as f:
-        json.dump(response_cmd, f, indent=5)
+def write_response_cmds(rsp_cmds: ResponseCommands):
+    with open(RESPONSE_COMMANDS_FILE_PATH, "w") as f:
+        json.dump(rsp_cmds, f, indent=2)
 
 
 class MySkype(SkypeEventLoop):
+    def merge_to_rsp_chats(self, changed_rsp_chats: dict[str, ResponseCommands]):
+        for chat, rsp_cmds in changed_rsp_chats.items():
+            self._rsp_chats[chat] = self._rsp_chats.setdefault(chat, {}) | rsp_cmds
+
+    def rsp_cmd(self, name: str) -> ResponseCommand:
+        return self._rsp_cmds.setdefault(name, {})
+
+    def rsp_chats(self, name: str | None) -> dict[str, ResponseCommands]:
+        if name is not None:
+            rsp_cmd = self.rsp_cmd(name)
+            return {chat: {name: rsp_cmd} for chat in rsp_cmd.setdefault("chats", [])}
+
+        return self._rsp_chats
+
+    def reset_rsp_cmd(self):
+        self._rsp_cmds = read_response_cmds()
+        self._rsp_chats: dict[str, ResponseCommands] = {}
+
+        for name in self._rsp_cmds.keys():
+            self.merge_to_rsp_chats(self.rsp_chats(name))
+
+    def __init__(self, *args, **kwargs):
+        super(MySkype, self).__init__(*args, **kwargs)
+        self.reset_rsp_cmd()
+
+    def save_rsp_cmd(self, name: str | None):
+        if name is not None:
+            changed_chats = self.rsp_chats(name)
+            self.merge_to_rsp_chats(changed_chats)
+
+        write_response_cmds(self._rsp_cmds)
+
     def onEvent(self, event):
         if isinstance(event, SkypeNewMessageEvent):
             self.handle_new_message_event(event)
@@ -76,10 +120,13 @@ class MySkype(SkypeEventLoop):
             if msg.startswith("!"):
                 self.handle_self_commands(event, quote, msg)
 
+        if event.msg.chat.id in self.rsp_chats():
+            self.handle_response_triggers(event)
+
     def handle_self_commands(
         self, event: SkypeNewMessageEvent, quote: Tag | None, msg: str
     ):
-        print(">> ", msg)
+        print(">>", msg)
         cmd = msg[1:]
 
         event.msg.delete()
@@ -116,28 +163,39 @@ class MySkype(SkypeEventLoop):
 
         cmd = cmd.split()
 
+        if cmd[:2] == ["rsp", "reload"]:
+            self.reset_rsp_cmd()
+
+        if cmd[:2] == ["rsp", "print"]:
+            event.msg.chat.sendMsg(
+                "_rsp_cmds = " + json.dumps(self._rsp_cmds, indent=4)
+            )
+            event.msg.chat.sendMsg(
+                "_rsp_chats = " + json.dumps(self._rsp_chats, indent=4)
+            )
+
         if cmd[:2] == ["rsp", "quote"] and quote is not None:
             name = cmd[2]
 
-            response_cmd = read_response_cmd(name)
-            rsp = response_cmd[name].setdefault("response", {})
+            rsp_cmd = self.rsp_cmd(name)
+            rsp = rsp_cmd.setdefault("response", {})
 
             rsp["quote"] = json.dumps(str(quote))
-            write_response_cmd(response_cmd)
+            self.save_rsp_cmd(name)
 
         if cmd[:2] == ["rsp", "trigger"]:
             name = cmd[2]
             trigger = " ".join(cmd[3:])
 
-            response_cmd = read_response_cmd(name)
-            triggers = response_cmd[name].setdefault("triggers", [])
+            rsp_cmd = self.rsp_cmd(name)
+            triggers = rsp_cmd.setdefault("triggers", [])
             triggers.append(trigger)
 
             unique = dict.fromkeys(triggers)
             triggers.clear()
             triggers.extend(unique)
 
-            write_response_cmd(response_cmd)
+            self.save_rsp_cmd(name)
 
         if cmd[:2] == ["rsp", "triggerchat"]:
             name = cmd[2]
@@ -146,15 +204,15 @@ class MySkype(SkypeEventLoop):
             if quote is not None:
                 new_chats.append(quote["conversation"])
 
-            response_cmd = read_response_cmd(name)
-            chats = response_cmd[name].setdefault("chat", [])
+            rsp_cmd = self.rsp_cmd(name)
+            chats = rsp_cmd.setdefault("chats", [])
             chats.extend(new_chats)
 
             unique = dict.fromkeys(chats)
             chats.clear()
             chats.extend(unique)
 
-            write_response_cmd(response_cmd)
+            self.save_rsp_cmd(name)
 
         if cmd[:2] == ["rsp", "file"]:
             raise NotImplementedError()
